efb803e7a1808a78fc6f078687120aca9156f77e
piotras <piotrek.pokora@gmail.com>
2012-04-20 08:40:00 +0200

    Added peoprties and method

--- src/Midgard/PHPCR/Query/SelectData/Executor.php
+++ src/Midgard/PHPCR/Query/SelectData/Executor.php
@@ -5,10 +5,12 @@ abstract class Executor
 {
     protected $holder = null;
     protected $query = null;
-    protected $result = null; 
+    protected $result = null;
+    protected $executors = null; 
 
     public abstract function executeQuery();
     public abstract function getQueryResult();
+    public abstract function getExecutors(); 
     //public abstract function getGuid();
     //public abstract function getName();
     //public abstract function getValue();
